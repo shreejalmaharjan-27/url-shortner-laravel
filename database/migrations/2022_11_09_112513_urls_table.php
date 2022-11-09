3c36dfc5702df5601f3de94ecfd963532ca175d7
shreejalmaharjan-27 <shreejal@rafled.com>
2022-11-09 11:46:53 +0000

    design database layout

---
+++ database/migrations/2022_11_09_112513_urls_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('urls', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('short')->unique();
+            $table->string('long');
+            $table->bigInteger('created_by')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('urls');
+    }
+};
