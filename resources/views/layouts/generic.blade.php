0d5d8b5c3576d64f59be3f986dd9ebc9d75edd3d
shreejalmaharjan-27 <shreejal@rafled.com>
2022-11-10 04:15:49 +0000

    create layout elements

---
+++ resources/views/layouts/generic.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @include('layouts.head')
+    @yield('head')
+</head>
+<body>
+    @include('layouts.header')
+    @yield('body')
+</body>
+</html>
