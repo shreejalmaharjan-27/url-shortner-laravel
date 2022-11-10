d98fe86f16751dfb9604ec0e1d8343d445c00f99
shreejalmaharjan-27 <shreejal@rafled.com>
2022-11-10 04:16:13 +0000

    create generator api

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\urlGenerator;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::controller(urlGenerator::class)->group(function () {
+    Route::post('generate', 'generate')->name('url.generate');
 });
