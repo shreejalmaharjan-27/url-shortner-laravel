6dbfe6415aa82d53a1c318c826eaf88a22c2df31
shreejalmaharjan-27 <shreejal@rafled.com>
2022-11-25 09:49:30 +0000

    add daisyui and use random theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
-      "./resources/**/*.blade.php",
-      "./resources/**/*.js",
-      "./resources/**/*.vue",
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
     ],
-    theme: {
-      extend: {},
+    daisyui: {
+        themes: [
+            {
+                mytheme: {
+                    "primary": "#ed9c10",
+                    "secondary": "#058e5c",
+                    "accent": "#b5ffad",
+                    "neutral": "#27223A",
+                    "base-100": "#F1ECF3",
+                    "info": "#609AE6",
+                    "success": "#5EDECB",
+                    "warning": "#FCAD45",
+                    "error": "#F2634A",
+                },
+            },
+        ],
     },
-    plugins: [],
-  }
+    plugins: [
+        require('@tailwindcss/typography'),
+        require("daisyui")
+    ],
+}
