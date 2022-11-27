8ab4d5fa96e5df2b08afce37622d95f06d1587b7
shreejalmaharjan-27 <shreejal@rafled.com>
2022-11-27 12:18:36 +0000

    add clipboard js, and show success message on successful copy to clipboard

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,8 @@
 import './bootstrap';
+import ClipboardJS from 'clipboard';
+// any element with class copy will instantiate
+const clipboard = new ClipboardJS('.copy');
+
+clipboard.on('success', (e) => {
+    alert('Copied link to clipboard.','success');
+});
