685357348d810ace90fa635056c4c6db2d06a027
shreejalmaharjan-27 <shreejal@rafled.com>
2022-11-27 03:18:34 +0000

    add tailwindcss line clamp plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -24,6 +24,7 @@ module.exports = {
     },
     plugins: [
         require('@tailwindcss/typography'),
-        require("daisyui")
+        require('@tailwindcss/line-clamp'),
+        require("daisyui"),
     ],
 }
